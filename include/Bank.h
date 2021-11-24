e00ceea819a15cd64799e3ebc39d55008b82f66c
Mehrnaz <mehrnazjiryaie@gmail.com>
2021-11-24 11:13:35 +0330

    Commit 1.5

--- include/Bank.h
+++ include/Bank.h
@@ -6,16 +6,16 @@
 class customer
 {
 private:
-    string username;           // account username
+    std::string username;           // account username
     int card_number;           // a four digit number
     long int oppening_date;    // not sure about its type
     long int expiration_date;  // not sure about its type
     unsigned long int balance; // account balance
     struct transaction;        // account transaction
-    string ip;                 // first account ip
-    vector<string> ips;         // account ips
+    std::string ip;                 // first account ip
+    std::vector<std::string> ips;         // account ips
 public:
-    customer(string, string);
+    customer(std::string, std::string);
     ~customer();
 };
 
--- src/Bank.cpp
+++ src/Bank.cpp
@@ -2,10 +2,34 @@
 #include "Bank.h"
 using namespace std;
 
-customer::customer(string UN, string IP)
+customer::customer(string UN, string IP) // constructor with two parameter
     : username(UN), ip(IP)
 {
+    try
+    {
+        if (!('a' <= username[0] <= 'z') || !('A' <= username[0] <= 'Z'))
+        {
+            throw 505;
+        }
 
+        for (size_t i = 1; i < username.size(); i++) // i = 0 was checked recently
+        {
+            if (!(0 <= username[i] <= 9) || !('a' <= username[i] <= 'z') || !('A' <= username[i] <= 'Z'))
+            {
+                throw "Invalid username!! - username must include upper or lower case or integer numbers";
+            }
+        }
+    }
+
+    catch (int num)
+    {
+        cerr << "Invalid username!! - username must start with an letter." << '\n';
+    }
+
+    catch (string msg)
+    {
+        cout << msg << endl;
+    }
 }
 
 customer::~customer()
